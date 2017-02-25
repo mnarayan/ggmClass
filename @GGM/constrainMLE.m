3cd73b1418b1b2e0f06ad9be46ad2667529261de
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-02-24 19:23:35 -0800

    Did not work due to variable naming problems. Function call works now. #bugfix

--- @GGM/constrainMLE.m
+++ @GGM/constrainMLE.m
@@ -1,4 +1,4 @@
-function [Theta results] = constrainMLE(Sigma,InitialTheta,varargin);
+function [Theta results] = constrainMLE(SigmaHat,InitialTheta,varargin);
 	% Relaxed or Constrained or Re-fitted MLE
 	% Maybe make this a twostageMLE of any kind?
 	% Inputs:
@@ -16,11 +16,12 @@ function [Theta results] = constrainMLE(Sigma,InitialTheta,varargin);
 	end
 	
 	if(nlambdas==1)
-		L_w = zeros(p,p);
-		L_w = 1./abs(InitialTheta); L_w = L_w/trace(L_w);
-		L_w(InitialTheta==0) = 1000; L_w(find(eye(p))) = 0;
+		Lambda = Lrange(1);
+		Lw = zeros(p,p);
+		Lw = 1./abs(InitialTheta); Lw = Lw/trace(Lw);
+		Lw(InitialTheta==0) = 1000; Lw(find(eye(p))) = 0;
 
-		[Theta SigmaHat2] = QUIC('default', SigmaHat, Lambda*L_w, 1e-6, 0, 1000);
+		[Theta SigmaHat2] = QUIC('default', SigmaHat, Lambda*Lw, 1e-6, 0, 1000);
 	
 		results.Lw = Lw;
 		results.Sigma = SigmaHat2;
@@ -28,10 +29,10 @@ function [Theta results] = constrainMLE(Sigma,InitialTheta,varargin);
 	elseif(nlambdas>1)
 		
 		% Constrained/Adaptive MLE 
-		L_w = ones(p,p);
-		L_w(self.InitialTheta==0) = 1000; L_w(find(eye(p))) = 0;
+		Lw = ones(p,p);
+		Lw(self.InitialTheta==0) = 1000; Lw(find(eye(p))) = 0;
 
-		[Theta SigmaHat3] = QUIC('path', SigmaHat, L_w, Lrange, 1e-6, 0, 1000);;
+		[Theta SigmaHat3] = QUIC('path', SigmaHat, Lw, Lrange, 1e-6, 0, 1000);;
 	
 		results.Lw = Lw;
 		results.Sigma = SigmaHat3;
