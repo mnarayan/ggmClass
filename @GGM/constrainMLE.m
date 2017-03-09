67a5e69e114dccce3b0c3e7e612fdbce0731a909
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-03-09 11:47:03 -0800

    Some cleaning up of calls to QUIC. All adaptive estimation done via constrainMLE()

--- @GGM/constrainMLE.m
+++ @GGM/constrainMLE.m
@@ -15,27 +15,48 @@ function [Theta results] = constrainMLE(SigmaHat,InitialTheta,varargin);
 		Lrange = [];
 	end
 	
+	getmaxlambda = @(x)(max(max(triu(abs(x)))));
+	lambda_max = getmaxlambda(SigmaHat); 
+	
+	
+	function Lw = getLambdaWeight(Theta,method)
+		
+		Lw  = [];
+		
+		switch method 
+			
+		case 'binary'
+			Lw = ones(p,p); 
+		case 'inv'	
+			Lw = 1./abs(Theta);		Lw = Lw/trace(Lw);
+		case 'invsq'
+			Lw = 1./abs(Theta).^2; 	Lw = Lw/trace(Lw);
+			
+		end
+		
+		nonzero_supp =  1.*(abs(Theta)>1e-5); 
+		Lw(abs(nonzero_supp)==0) = 100*lambda_max; Lw(find(eye(p))) = 0;
+	end
+	
+	
 	if(nlambdas==1)
+		
+		% Adaptive
 		Lambda = Lrange(1);
-		Lw = zeros(p,p);
-		Lw = 1./abs(InitialTheta); Lw = Lw/trace(Lw);
-		Lw(InitialTheta==0) = 1000; Lw(find(eye(p))) = 0;
-
+		Lw = getLambdaWeight(InitialTheta,'inv'); 
 		[Theta SigmaHat2] = QUIC('default', SigmaHat, Lambda*Lw, 1e-6, 0, 1000);
 	
 		results.Lw = Lw;
 		results.Sigma = SigmaHat2;
 	
 	elseif(nlambdas>1)
-		
-		% Constrained/Adaptive MLE 
-		Lw = ones(p,p);
-		Lw(self.InitialTheta==0) = 1000; Lw(find(eye(p))) = 0;
 
-		[Theta SigmaHat3] = QUIC('path', SigmaHat, Lw, Lrange, 1e-6, 0, 1000);;
+		% Constrained
+		Lw = getLambdaWeight(InitialTheta,'inv'); 
+		[Theta SigmaHat2] = QUIC('path', SigmaHat, Lw, Lrange, 1e-6, 0, 1000);
 	
 		results.Lw = Lw;
-		results.Sigma = SigmaHat3;
+		results.Sigma = SigmaHat2;
 	
 	end
 	
--- @GGM/sparseMLE.m
+++ @GGM/sparseMLE.m
@@ -1,7 +1,6 @@
 function [self results] = sparseMLE(self,varargin)
 	% 
 	
-	objfields = fieldnames(self);
 	
 	[m p n] = size(self.Data);
 	SigmaHat = self.Sigma;
@@ -11,12 +10,11 @@ function [self results] = sparseMLE(self,varargin)
 	if(self.verbose)
 		SigmaHat(1:5,1:5)
 		disp('Maximum off-diagonal in S1')
-		max(max(triu(SigmaHat,1)))
+		max(max(triu(abs(SigmaHat),1)))
 		%disp('340')
 		pause(.01)	
 	end
 
-		%%%%%%%%% Estimate Group Specific Global Graph with whitened Sigma %%%%%%%%%%
 	if(Lambda~=0)
 		
 		if(self.verbose)
@@ -29,13 +27,14 @@ function [self results] = sparseMLE(self,varargin)
 		end
 		
 		[Thetahat0 SigmaHat2] 		=  QUIC(...
-										'default', ...
-										SigmaHat,  ...
-										Lambda,  ...
-										1e-6,  ...
-										0,  ...
+										'default', 	...
+										SigmaHat,  	...
+										Lambda,		...
+										1e-6,  		...
+										0,  		...
 										1000);
 		self.Theta = Thetahat0;
+		results.SigmaHat = SigmaHat2;
 
 		% % Relaxed/Refitted MLE
 		% L_w = zeros(p,p);
@@ -49,12 +48,13 @@ function [self results] = sparseMLE(self,varargin)
 		[self.AdaptTheta results] = GGM.constrainMLE(...
 									SigmaHat, ...
 									Thetahat0, ...
-									Lambda); 
-		
-		results.SigmaHat2 = SigmaHat2;
+									1.0); 
+		self.Wt = results.Lw;
+	
+		results.constrainMLE = results;
 				
 		clear Thetahat0 Thetahat1 SigmaHat2;
-			
+		
 		if(self.verbose)
 			disp('Done ...')
 			pause(.01)
@@ -62,7 +62,7 @@ function [self results] = sparseMLE(self,varargin)
 
 	elseif(Lambda==0)
 		
-		assertEqual(Lambda,0,'Lambda is not 0 in path mode');
+		assert(Lambda==0,'Lambda should be 0 in path mode');
 		 
 		if(self.verbose) 
 			disp('Lambda not provided ... ');
@@ -85,30 +85,30 @@ function [self results] = sparseMLE(self,varargin)
 		end
 	
 		if(isreal(Lrange(1)))
-			[Thetahat0 SigmaHat2] = QUIC('path', SigmaHat, 1.0, Lrange, 1e-6, 0, 1000);			
+			[Thetahat0 SigmaHat2] = QUIC('path',  	...
+										SigmaHat,  	...
+										1.0,		...
+										Lrange,  	...
+										1e-6,  		...
+										0,  		...
+										1000);			
+										
 			% gr_sparsity = squeeze(sum(sum(Thetahat0~=0,1),2));
 			% allzero_idx = min(find((gr_sparsity==0)));
 			% self.Lrange = self.Lrange(1:allzero_idx);
 			% self.ThetaPath = Thetahat0(:,:,1:allzero_idx);
 			self.ThetaPath = Thetahat0;
 			self.Theta = [];
+			results.sparsity = squeeze(sum(sum(Thetahat0~=0,1),2));
+			results.Lrange = Lrange;
+			results.SigmaHat = SigmaHat2; 
 		else
 			error('Regularization values are complex')
 			self.ThetaPath = []
 			self.Theta = [];
 		end
 	
-		% % Constrained/Adaptive MLE
-		% Lrange = self.Lrange;
-		% L_w = ones(p,p);
-		% L_w(self.ThetaPath(:,:,20)==0) = 1000; L_w(find(eye(p))) = 0;
-		% self.Wt = L_w;
-		%
-		% [Thetahat1 SigmaHat3] = QUIC('path', SigmaHat, L_w, Lrange, 1e-6, 0, 1000);
-		% self.AdaptTheta=Thetahat1;
-		% results.SigmaHat3 = SigmaHat3;
 	
 	end
-	
 	
 end
