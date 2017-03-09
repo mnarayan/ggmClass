8545dd5a09e2c44b261092885030a00ffce1ccee
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-03-09 11:56:05 -0800

    Static method to debias any network estimate

---
+++ @GGM/debias.m
@@ -0,0 +1,30 @@
+function [varargout] = debias(self,varargin)
+%DEBIAS returns debiased sparse inverse covariance estimate using the  Jankova-VanDeGeer Method
+% 	
+% 	SYNTAX: 
+% 	Call on a GGM object instance self
+% 		self.debias()
+% 	Call using an options structure 
+% 		output = ggm.debias(options)
+% 
+
+	contents = fieldnames(self); 
+	if(~sum(strcmp(contents,'Theta'))|~sum(strcmp(contents,'Sigma')))
+		mfile_showhelp;
+		error(sprintf('self.Theta and self.Sigma needed'));
+	end	
+
+	hessianProj = @(Sigma,Theta)(Theta'*Sigma*Theta);
+	debiaser 	= @(Sigma,Theta)(Theta + Theta' - hessianProj(Sigma,Theta)); 
+	
+	if(isobject(self))	
+		sprintf('This is a %s class object',mfilename('class'));		
+		self.Theta = debiaser(self.Sigma,self.Theta);	
+	else
+		nargoutchk(1);
+		self.Theta_debias = debiaser(self.Sigma,options.Theta);		
+		varargout{1} = self;		
+	end
+
+
+end
