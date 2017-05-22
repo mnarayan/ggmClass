a00c830edce03fe77d4c459c2940d2bd4f3c487b
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-05-21 18:42:22 -0700

    Fixed bugs. Modified to ensure shared factor is in fact low rank

--- +covariance/common_lowrank_decomposition.m
+++ +covariance/common_lowrank_decomposition.m
@@ -30,8 +30,11 @@ function [Sigma Sigma_i] = common_lowrank_decomposition(A,options)
 options.scaled = true;
 options.mode = 'X'; 
 options.sampleCovariance = @(X)(covariance.mle_sample_covariance(X)); 
-options.poolCovariance = @(Si,ni)(sum(Si.*repmat(ni,[size(Si,1) size(Si,2)]),3)/sum(ni)); 
-options.projectCovariance = @projection_tangentspace;
+options.poolCovariance = @(Si,ni)(sum(Si.*repmat(ni,[size(Si,1) size(Si,2) 1]),3)/sum(ni)); 
+options.projectCovariance = @covariance.projection_tangentspace;
+options.max_iter = 100;
+options.err_tol = 1e-5;
+
 
 %  Fixed point algorithm: 
 lr_dim = 1;
@@ -39,30 +42,59 @@ options.lr_dim = lr_dim;
 % Initialize
 Si = []; ni=[];
 if(strcmp(options.mode,'X'))
+    [n p s] = size(A);
     for cc=1:s
         Si(:,:,cc) = options.sampleCovariance(A(:,:,cc)); 
-        ni(cc) = size(A,1); 
+        ni(1,1,cc) = size(A,1); 
     end
 else
+    [~, p, s] = size(A);
     Si = A;
     % What is ni in this case? 
-    ni = 1/size(Si,3); 
-end
-clear A; 
-
-hatSigma = options.poolCovariance(Si,ni); 
-[V D] = eig(hatSigma); 
-[~ lr_idx] = sort(D,'descend'); 
-B = V(:,lr_idx(1:lr_dim)) * diag(sqrt(D(lr_idx(1:lr_dim)))); % Shared low rank
-
-
-options.projectfun = @(S)(B*inv(B'*S*B)*B'*S); 
-
-for cc=1:s
-    hatSigma_i(:,:,cc) = options.projectCovariance(...
-                                    projectfun,  ...
-                                    S(:,:,cc),  ...
-                                    hatSigma);
+    ni = 1/size(Si,3).*ones(1,1,cc); 
 end
 
+%clear A; 
 
+kk=0;
+conv_err = Inf;
+oldSig = options.poolCovariance(Si,ni)- eye(size(Si,2)); 
+oldSig_i = Si; 
+newSi = zeros(size(Si)); 
+
+while((kk<options.max_iter) && (conv_err>=options.err_tol))
+    
+    hatSigma = oldSig + options.poolCovariance(newSi,ni); 
+    [V D] = eig(hatSigma); 
+    [~,lr_idx] = sort(D,'descend'); 
+    B = V(:,lr_idx(1:lr_dim)) * diag(sqrt(D(lr_idx(1:lr_dim)))); % Shared low rank
+
+    options.projectfun = @(S)(B*inv(B'*S*B)*B'*S); 
+    for cc=1:s
+        [hatSigma_i(:,:,cc) ResProjSigma] = options.projectCovariance(...
+                                        options.projectfun,  ...
+                                        oldSig_i(:,:,cc),  ...
+                                        hatSigma);
+    end
+    
+    % Check convergence    
+    err_Sig = norm(hatSigma-oldSig,'fro')/numel(oldSig);
+    err_Sigi = sum(sum(sum((hatSigma_i-oldSig_i).^2,3)))/numel(oldSig_i); 
+     
+    if(kk<=5 | mod(kk,5)==0)
+        disp(sprintf('Iter:%d, err:%.5f',kk+1,err_Sig)); 
+    end    
+    conv_err = err_Sig;
+    
+    % Update Individual Covariances;
+    %projS = options.projectfun(hatSigma); 
+    %newSi = Si-repmat(projS'*hatSigma*projS, [1 1 s]) + hatSigma_i;
+    newSi = Si-hatSigma_i; %+ repmat(hatSigma,[1 1 s]);
+    oldSig_i = hatSigma_i;
+    oldSig = hatSigma;
+    kk=kk+1;
+
+end
+
+Sigma = hatSigma;
+Sigma_i = hatSigma_i;
