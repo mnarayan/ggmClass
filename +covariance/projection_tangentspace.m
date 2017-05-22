fb4f36402b4b6288ec59d8a53265b3e4a0761c20
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-05-21 18:44:05 -0700

    estimate individual covariance orthogonal to shared factor

--- +covariance/projection_tangentspace.m
+++ +covariance/projection_tangentspace.m
@@ -1,4 +1,4 @@
-function [OrthProjSigma] = projection_tangentspace(projectfun, Si, S); 
+function [OrthProjSigma ResProjSigma] = projection_tangentspace(projectfun, Si, S); 
 %PROJECTION_TANGENTSPACE: Given matrices and their frechet mean, returns projection of each matrix onto tangent direction to frechet mean
 % 
 % INPUT
@@ -7,6 +7,17 @@ function [OrthProjSigma] = projection_tangentspace(projectfun, Si, S);
 %   S           - frechet mean covariance or covariance in some shared subspace
 % 
     projS = projectfun(S);
-    OrthProjSigma = projS'*(Si-S)*projS; 
-
+    %OrthProjSigma = projS'*(Si-S)*projS; 
+    OrthProjSigma = zeros(size(Si)); 
+    if(ndims(Si)==3)
+        for cc=1:size(Si,3)
+            OrthProjSigma(:,:,cc) = Si(:,:,cc)-projS'*(Si(:,:,cc))*projS;
+        end
+    else
+        %disp('Single projection')
+        %OrthProjSigma = Si-projS'*(Si)*projS;
+        ResProjSigma = projS'*(Si)*projS;
+        OrthProjSigma = Si - ResProjSigma;
+    end
+    
 end
