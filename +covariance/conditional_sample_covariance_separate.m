4acc36e96f618c690ad1e7f30a8d5ab3ab9fcea0
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-06-12 16:16:26 -0400

    Fixed conditional correlation bugs

--- +covariance/conditional_sample_covariance_separate.m
+++ +covariance/conditional_sample_covariance_separate.m
@@ -35,10 +35,11 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 	% ~isfield(options,'verbose')
 	
 	if(~isfield(options,'corrfun'))
-		options.corrfun = @(X)(covariance.mle_sample_covariance( ...
-									X, ...
-									struct('standardize',false) ...
-									));
+        %options.corrfun = @(X)(cov(X));
+        options.corrfun = @(X)(covariance.mle_sample_covariance( ...
+                                    X, ...
+                                    struct('standardize',false) ...
+                          ));
 	end
 	
 	function Sig = my_xcorr(X,y,options)
@@ -57,54 +58,71 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 		end
 		
 		if(options.standardize)
-			Xt = standardize.successive_normalize(X');
-			X  = Xt'; 
-			if(q>1)
-				y = standardize.successive_normalize(y); 	
-			else
-				y = standardize.standardize_cols(y); 	
-			end
+            % Xt = standardize.successive_normalize(X');
+            % X  = Xt';
+            X = standardize.standardize_cols(X);	                 	
+            if(q>1)
+                %y = standardize.successive_normalize(y);
+    			[~, mu_y, sig_y] = standardize.standardize_cols(y); 
+            else
+    			[~, mu_y, sig_y] = standardize.standardize_cols(y); 
+            end
 			Sig = X'*y/n1;
+            Sig = Sig*diag(1./sig_y); 
 					
 		else
 			[~, mu_x, sig_x] = standardize.standardize_cols(X); 
-			[~, mu_y, sig_y] = standardize.standardize_cols(y); 
+			[~, ~, sig_y] = standardize.standardize_cols(y); 
 			%y = zscore(y);
 			
 			if(q>1)
-				Sig = bsxfun(@minus,X,mu_x)'*bsxfun(@minus,y,mu_y)/n1; 
+				Sig = bsxfun(@minus,X,mu_x)'*y/n1; 
 			elseif(q==1)
-				Sig = bsxfun(@minus,X,mu_x)'*(y-mu_y)/n1; 
+				Sig = bsxfun(@minus,X,mu_x)'*y/n1; 
 			end
 			
-			Sig = diag(1./sig_x)*Sig*diag(1./sig_y);			
-			
+			Sig = diag(1./sig_x)*Sig;
+			%Sig = diag(1./sig_x)*Sig*diag(1./sig_y);			
+            			
 		end
-		
 		
 	end
 	
+    proj = @(X)(X*pinv(X'*X)*X');
 	if(~isfield(options,'xcorrfun'))
-		options.xcorrfun = @(X,y)(my_xcorr(X,y,struct('standardize',false)));
+		options.xcorrfun = @(X,y)(my_xcorr(X,y,struct('standardize',true)));
+        %options.xcorrfun = @(X,y)(proj(y)*standardize.standardize_cols(X)); 
 	end
 	
 	if(~isfield(options,'nuisance'))
 		options.nuisance = mean(X,2); 
 	end
 	
-	y = options.nuisance;
+	y = options.nuisance;    
 	Sig_xx = options.corrfun(X); 		
 	Sig_xy = options.xcorrfun(X,y); 
 	Sig_yy = options.corrfun(y);
-	SigmaL = Sig_xy*inv(Sig_yy)*Sig_xy';
-	Sigma = Sig_xx - SigmaL; 
+    if(size(Sig_xy,2)==size(Sig_yy,1))
+        tmpSigmaL = Sig_xy*pinv(Sig_yy)*Sig_xy';
+        [LV LD] = eig(tmpSigmaL);    
+        SigmaL = LV(:,1:5)*real(LD(1:5,1:5))*LV(:,1:5)';
+    else
+        tmpSigmaL = cov(Sig_xy); 
+        SigmaL = tmpSigmaL;
+    end    
+
+    % Remove low rank component
+    Sigma = Sig_xx - SigmaL;
+    varcorr = @(D,Sig)(diag(1./D)*Sig*diag(1./D))        
+    Sigma = varcorr(sqrt(diag(Sigma)),Sigma);
 	
 	results.SigXX = Sig_xx;
 	results.SigXY = Sig_xy;
 	results.SigYY = Sig_yy;
 	results.nCov  = SigmaL;
+    results.nCorr = covariance.var_corr(SigmaL);
 	results.Sigma = Sigma;
-	results.NSR   = trace(SigmaL)/trace(Sig_xx);
+	results.NSR   = trace(SigmaL)/trace(Sig_xx)
 	results.Y = y;
 	
 	function Xy =  Xy_orthogonalize(X,Y)
@@ -119,21 +137,21 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 		% end
 		
 		mu = mean(X); 
-		Xcen = bsxfun(@minux,X,mu); 
-		
+		Xcen = bsxfun(@minus,X,mu); 
+        
 		proj = @(X)(X*pinv(X'*X)*X');
 		Xy = Xcen - proj(Y)*Xcen;
 		Xy = bsxfun(@plus,Xy,mu);
 		 
 	end
 
-	results.X_perpY = Xy_orthogonalize(X,options.nuisance);
+	results.X_perpY = Xy_orthogonalize(standardize.standardize_cols(X),options.nuisance);
 	
 	
 	if(options.verbose)
 		% Only perform for comparison purposes;
-		gsr = options.nuisance; 				
-		Xgsr = Xy_orthogonalize(X,gsr); 
+		nuisance = options.nuisance; 				
+		Xgsr = Xy_orthogonalize(standardize.standardize_cols(X),nuisance); 
 		
 		figure; 
 		subplot(2,2,1); 
@@ -148,7 +166,8 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 		subplot(2,2,4); 
 		imagesc(options.corrfun(Xgsr)); colorbar; axis equal image; 
 		title('Correlation (NSR)');
-		print('-dpng','-r300',fullfile(options.outputdir,options.filename)); 			
+        fullfile(options.outputdir,options.filename)
+		print('-dpdf','-r150',fullfile(options.outputdir,options.filename)); 		pause(5)	
 		close all;
 	end	
 	
