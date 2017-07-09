a4431a8e79e6fd271f8edfc2dcc65c78fec2d680
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-07-09 13:46:46 -0700

    Fixed bug to handle 0 variance features if needed

--- +covariance/conditional_sample_covariance_separate.m
+++ +covariance/conditional_sample_covariance_separate.m
@@ -1,7 +1,9 @@
 function [Sigma results] = conditional_sample_covariance_separate(X,options)
 %CONDITIONAL_SAMPLE_COVARIANCE_SEPARATE
 % 
-% Schur complement using correlation decomposition
+% Sigma = Sigma_obs - Sigma_nuisance
+% 
+% Obtained by 
 % Schur complement using covariance decomposition
 % Residuals after regressing out confounds and modified cholesky decomposition 
 % 	
@@ -13,7 +15,13 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 % 	- options.verbose: Plot and print outputs
 % 	- options.outputdir: Defaults to 'tmp/'
 % 	- options.filename: Defaults to 'conditionalCov'
-	
+% 
+% OUTPUT
+% - results.Sigma - Denoised Correlation Matrix
+% - results.nCov - Nuisance Covariance Matrix
+% - results.NSR - Nuisance to Observed Signal Ratio (trace(nCov)/trace(Sigma_obs))
+% - results.X_perpY - Projection of Data onto orthogonal complement of nuisance Y
+% - results.nCorr - Rescaled Nuisance Correlation Matrix	
 	
 	if(~exist('options','var'))
 		options = []
@@ -68,6 +76,7 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
     			[~, mu_y, sig_y] = standardize.standardize_cols(y); 
             end
 			Sig = X'*y/n1;
+            sig_y(sig_y<=0) = 1;
             Sig = Sig*diag(1./sig_y); 
 					
 		else
@@ -81,6 +90,7 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 				Sig = bsxfun(@minus,X,mu_x)'*y/n1; 
 			end
 			
+            sig_x(sig_x<=0) = 1;
 			Sig = diag(1./sig_x)*Sig;
 			%Sig = diag(1./sig_x)*Sig*diag(1./sig_y);			
             			
@@ -90,8 +100,8 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 	
     proj = @(X)(X*pinv(X'*X)*X');
 	if(~isfield(options,'xcorrfun'))
-		options.xcorrfun = @(X,y)(my_xcorr(X,y,struct('standardize',true)));
-        %options.xcorrfun = @(X,y)(proj(y)*standardize.standardize_cols(X)); 
+		%options.xcorrfun = @(X,y)(my_xcorr(X,y,struct('standardize',true)));
+        options.xcorrfun = @(X,y)(proj(y)*standardize.standardize_cols(X)); 
 	end
 	
 	if(~isfield(options,'nuisance'))
@@ -104,8 +114,10 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 	Sig_yy = options.corrfun(y);
     if(size(Sig_xy,2)==size(Sig_yy,1))
         tmpSigmaL = Sig_xy*pinv(Sig_yy)*Sig_xy';
-        [LV LD] = eig(tmpSigmaL);    
-        SigmaL = LV(:,1:5)*real(LD(1:5,1:5))*LV(:,1:5)';
+        [LV LD] = eig(tmpSigmaL);
+        [sortLD, LDidx] = sort(diag(LD),'descend'); LDidx = LDidx(real(sortLD)>0);    
+        SigmaL = LV(:,LDidx)*real(LD(LDidx,LDidx))*LV(:,LDidx)';
+        SigmaL = real(SigmaL);
     else
         tmpSigmaL = cov(Sig_xy); 
         SigmaL = tmpSigmaL;
@@ -113,17 +125,20 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 
     % Remove low rank component
     Sigma = Sig_xx - SigmaL;
-    varcorr = @(D,Sig)(diag(1./D)*Sig*diag(1./D))        
-    Sigma = varcorr(sqrt(diag(Sigma)),Sigma);
+    varcorr = @(D,Sig)(diag(1./D)*Sig*diag(1./D));
+    D = real(diag(Sigma)); 
+    D(D<=0) = 1; 
 	
 	results.SigXX = Sig_xx;
 	results.SigXY = Sig_xy;
 	results.SigYY = Sig_yy;
 	results.nCov  = SigmaL;
-    results.nCorr = covariance.var_corr(SigmaL);
-	results.Sigma = Sigma;
-	results.NSR   = trace(SigmaL)/trace(Sig_xx)
-	results.Y = y;
+    results.Sigma = varcorr(sqrt(D),Sigma);
+	results.SigmaCov = Sigma; %
+    results.nCorr = varcorr(sqrt(diag(SigmaL)),SigmaL);
+	results.NSR = trace(SigmaL)/trace(Sig_xx);
+    results.SNR = trace(Sigma)/trace(SigmaL);
+    results.Y = y;
 	
 	function Xy =  Xy_orthogonalize(X,Y)
 	% Nuisance signal regression
@@ -153,15 +168,22 @@ function [Sigma results] = conditional_sample_covariance_separate(X,options)
 		nuisance = options.nuisance; 				
 		Xgsr = Xy_orthogonalize(standardize.standardize_cols(X),nuisance); 
 		
+    	if(exist('brewermap'))	
+    		colormapfun = @()(brewermap(length(colormap),'RdYlBu'));
+    		close all;
+    	else
+    		colormapfun = @winter;
+    	end
+        
 		figure; 
 		subplot(2,2,1); 
 		imagesc(Sig_xx); colorbar; axis equal image; 
 		title('Correlation'); 
 		subplot(2,2,2); 
-		imagesc(Sigma); colorbar; axis equal image; 
+		imagesc(results.Sigma); colorbar; axis equal image; 
 		title('Conditional Corr.'); 
 		subplot(2,2,3);  
-		imagesc(SigmaL); colorbar; axis equal image; 
+		imagesc(results.nCov); colorbar; axis equal image; 
 		title('Nuisance Factor'); 
 		subplot(2,2,4); 
 		imagesc(options.corrfun(Xgsr)); colorbar; axis equal image; 
