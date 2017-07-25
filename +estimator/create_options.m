eb020c1e205c07d2239efc388044bf594b699361
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-07-24 20:01:15 -0700

    Additional options for lasso_mle

--- +estimator/create_options.m
+++ +estimator/create_options.m
@@ -1,22 +1,33 @@
 function options = create_options(varargin)
     
+    % Default what is the model 
+    options.model = 'gaussian';
+    
     % Default options for estimator
-    options.covfun = @(X)(covariance.mle_sample_covariance(X,...
+    options.covariancefun = @(X)(covariance.mle_sample_covariance(X,...
                                         struct('standardize','cols') ...
                                         ));
-    options.estimator = @sparseMLE;
+    options.init_estimator = 'lasso_mle';                                  
+    options.estimator = 'lasso_mle'
     options.solver = 'QUIC';
-    options.adaptive = true;
+    options.refit = false;
     
     % Default options for the regularization path
     options.get_lambda = @(Sigma)({...
-                            .01*min(min(abs(triu(Sigma(abs(Sigma)>0),1)))),...
-                            max(max(abs(triu(Sigma,1)))); ...
+                            .1*min(abs(Sigma(abs(triu(Sigma,1))>0))),...
+                            max(max(abs(triu(Sigma,1)))) ...
                             });
     options.lambda_min = .01;
     options.lambda_max = 1.0;
     options.nlambdas = 30;
-    options.lambdafun = @(lmin,lmax)(linspace(lmin,lmax,options.nlambda));
+    options.lambdafun = @(lmin,lmax)(fliplr(linspace(...
+                                            lmin,...
+                                            lmax,...
+                                            options.nlambdas...
+                                            )));
+    options.Lambda = 0;
+    options.W = [];
+    options.path = options.lambdafun(options.lambda_min,options.lambda_max);
     
     % Default options for creating resamples or subsamples
     options.resampler.options = @generator.create_options
@@ -29,5 +40,6 @@ function options = create_options(varargin)
     options.selection.score = @(Sigma,Theta)(stars(Theta));
     options.graphlet = 'dyad'; % edgelet
     options.instability_beta = .15;
+    
     
 end
