c2dd976618b11ef07646770f3c5e9b97326065f2
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-08-15 11:29:57 -0700

    Pass function arguments for both covariance and cross-covariance

--- +reliability/mccc.m
+++ +reliability/mccc.m
@@ -4,20 +4,26 @@ function [mccc Mccc] =  MCCC(X,Y,varargin)
 % USAGE: MCCC
 % 
 % INPUTS:
-% 	- X : A data matrix of n measurements and p features
-% 	- Y : A data matrix of n measurements and p features
+% 	- X : A data matrix of n i.i.d measurements and p features
+% 	- Y : A data matrix of n i.i.d measurements and p features
 % 	- options (optional)	
 % 
 % REFERENCES: 
 % Hiriote and Chinchilli, "Matrix-based concordance correlation coefficient for repeated measures", Biometrics. (2011)	
 % 
+% Copyright, 2017, Manjari Narayan
+% GPL-2 License
+% 
 
 	assert(size(X,1)==size(Y,1),'X and Y must have same number of rows'); 
 	assert(size(X,2)==size(Y,2),'X and Y must have same number of cols'); 
 
-    [mccc Mccc] = matrix_ccc(X,Y,@cov); 
+    crosscov = @(X,Y)(X'*Y/size(X,1));
 
-	function [mccc M_ccc] = matrix_ccc(X,Y,covfun)
+    [mccc Mccc] = matrix_ccc(X,Y,@cov,crosscov); 
+    
+
+	function [mccc M_ccc] = matrix_ccc(X,Y,covfun,crosscov)
         
         n = size(X,1); p = size(X,2);
         
@@ -26,9 +32,7 @@ function [mccc Mccc] =  MCCC(X,Y,varargin)
 
 		X = bsxfun(@minus,X,mu_x/n); 
 		Y = bsxfun(@minus,Y,mu_y/n);
-        
-        crosscov = @(X,Y)(X'*Y/size(X,1));
-		
+        		
 		V_ind = covfun(X) + covfun(Y) + (mu_x - mu_y)*(mu_x - mu_y)'/(n*(n-1));
         V_dep = V_ind - crosscov(X,Y) - crosscov(Y,X);
         
@@ -37,7 +41,6 @@ function [mccc Mccc] =  MCCC(X,Y,varargin)
         Vposdef = Veig(:,nonneg_eig);
         Dposdef = Deig(nonneg_eig,nonneg_eig);
         Vsqinv = Vposdef*diag(sqrt(1./diag(Dposdef)))*Vposdef';
-		
         
         M_rho = Vsqinv * V_dep * Vsqinv;
 		M_ccc = eye(p) - M_rho;
