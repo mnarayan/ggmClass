929b54d214a6f99cb43827ba3668fea576cdf7cb
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-08-16 09:58:48 -0700

    Added a test function for reliability.mccc

---
+++ test/test_mccc.m
@@ -0,0 +1,60 @@
+function test_suite = test_standardize
+	buildFunctionHandleTestSuite(localfunctions);
+
+end
+
+function test_null_unit_diagonal()
+   
+    n = 100; 
+    p = 5; 
+    ntrials = 100;
+    
+    Sigma = eye(2*p);
+    rng('default');
+    
+    mccc = nan(1,ntrials);
+    
+    for trialno=1:ntrials
+        AllData = randn(n,2*p)*sqrtm(Sigma);
+        X = AllData(:,1:p); 
+        Y = AllData(:,p+1:2*p); 
+        [mccc(trialno)] = reliability.mccc(X,Y);
+    end
+    
+    disp('Null Hyp. for Unit Diagonal')
+    disp(['n=' num2str(n) ' i.i.d measurements, p=' num2str(p) ' variables']);
+    disp('Mean mccc (100 trials)')
+    mean(mccc)
+    disp('MCMC stdev (100 trials)'); 
+    std(mccc)
+    
+end
+
+
+% function test_hiriote_example()
+%
+%     n = 100;
+%     p = 3;
+%     ntrials = 100;
+%
+%     Sigma = eye(2*p);
+%     rng('default');
+%
+%     mccc = nan(1,ntrials);
+%     mccc_ci = nan(2,ntrials);
+%
+%     for trialno=1:ntrials
+%         AllData = randn(n,2*p)*sqrtm(Sigma);
+%         X = AllData(:,1:p);
+%         Y = AllData(:,p+1:2*p);
+%         [mccc(trialno),~,mccc_ci(:,trialno)] = reliability.mccc(X,Y);
+%     end
+%
+%     disp('Null Hyp. for Unit Diagonal')
+%     disp(['n=' num2str(n) ' i.i.d measurements, p=' num2str(p) ' variables']);
+%     disp('Mean mccc (100 trials)')
+%     mean(mccc)
+%     disp('MCMC stdev (100 trials)');
+%     mean(mccc_ci,2)
+%
+% end
