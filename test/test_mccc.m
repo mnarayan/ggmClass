acae3fae73e69b0d71951af3dfe1d09c742bc326
Manjari Narayan (MBAir) <mnarayan@users.noreply.github.com>
2017-08-23 11:18:10 -0700

    Updated with additional tests for alternative

--- test/test_mccc.m
+++ test/test_mccc.m
@@ -31,7 +31,7 @@ function test_null_unit_diagonal()
 end
 
 
-function test_alt_unit_diagonal()
+function test_perfect_unit_diagonal()
    
     n = 100; 
     p = 5; 
@@ -46,6 +46,35 @@ function test_alt_unit_diagonal()
         AllData = randn(n,p)*sqrtm(Sigma);
         X = AllData(:,1:p); 
         Y = AllData(:,1:p);
+        [mccc(trialno)] = reliability.mccc(X,Y);
+    end
+    
+    disp('Null Hyp. for Unit Diagonal')
+    disp(['n=' num2str(n) ' i.i.d measurements, p=' num2str(p) ' variables']);
+    disp('Mean mccc (100 trials)')
+    mean(mccc)
+    disp('MCMC stdev (100 trials)'); 
+    std(mccc)
+    
+end
+
+function test_alt_unit_diagonal()
+   
+    n = 100; 
+    p = 5; 
+    ntrials = 100;
+    
+    Sigma = eye(2*p)*2*p;
+    Sigma(1:p,p+1:2*p) = .9;
+    Sigma(p+1:2*p,1:p) = .9;
+    rng('default');
+    
+    mccc = nan(1,ntrials);
+    
+    for trialno=1:ntrials
+        AllData = randn(n,2*p)*sqrtm(Sigma);
+        X = AllData(:,1:p); 
+        Y = AllData(:,p+1:2*p);
         [mccc(trialno)] = reliability.mccc(X,Y);
     end
     
