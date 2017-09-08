a7837f0457465285c31a90b2778527fe74dbb3fe
M. Narayan <mnarayan@noreply.users.github.com>
2017-09-07 19:21:50 -0700

    estimator.fit now performs ebic by default

--- +estimator/fit.m
+++ +estimator/fit.m
@@ -13,11 +13,16 @@ function [results] = fit(X,options)
         
 	case 'gaussian'
         [covariance_estimate, inverse_covariance_estimate, sample_covariance, fit_options] = estimator.penalized_mle_inverse_covariance(X,options);
-
 	otherwise
 		disp('Not yet supported');
 	end
-
+    
+    if((options.Lambda == 0) && (options.nlambdas > 1))
+        results.ebic = estimator.ebic(inverse_covariance_estimate,...
+                                        X,...
+                                        fit_options...
+                                        );
+    end
     results.covariance_estimate = covariance_estimate;
     results.inverse_covariance_estimate = inverse_covariance_estimate;
     results.sample_covariance = sample_covariance;
